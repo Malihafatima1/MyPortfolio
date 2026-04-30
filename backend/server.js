8b55576e569fe7b48c6395e20bc8ceca338d04f9
Maliha Fatima <maliha@localhost.localdomain>
2026-04-30 16:01:57 +0530

    Solved conflict

---
+++ backend/server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const twilio = require("twilio");
+const Contact = require("./models/Contact");
+
+const app = express();
+app.use(cors({
+  origin: "*"
+}));
+app.use(bodyParser.json());
+
+
+
+// ✅ MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+console.log("SID:", process.env.TWILIO_SID);
+console.log("AUTH:", process.env.TWILIO_AUTH);
+
+// ✅ Twilio
+const client = require("twilio")(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_AUTH
+);
+
+// ✅ Nodemailer
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend working");
+});
+
+// ✅ MAIN API
+app.post("/send-sms", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    // 1️⃣ Save to DB
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+
+    // 2️⃣ Send SMS
+    await client.messages.create({
+      body: `New Message:
+  Name: ${name}
+  Email: ${email}
+  Message: ${message}`,
+      from: process.env.TWILIO_PHONE,
+      to: process.env.MY_PHONE,
+    });
+
+    // 3️⃣ Send Email
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: "New Portfolio Message",
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    res.send("Saved + SMS + Email sent");
+  } catch (err) {
+    console.log("ERROR:", err);
+    res.status(500).send(err.message);
+  }
+});
+
+// ✅ Get all messages (Admin)
+app.get("/messages", async (req, res) => {
+  const messages = await Contact.find().sort({ date: -1 });
+  res.json(messages);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
